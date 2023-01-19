92e0d63690238e444d0d0d0a436d05d4683089ec
Fatih Dumanli <fatihdumanli@live.com>
2023-01-19 19:44:52 +0300

    Remove unused using statement.

--- src/MarginTrading.AccountsManagement/Services/IEventSender.cs
+++ src/MarginTrading.AccountsManagement/Services/IEventSender.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using MarginTrading.AccountsManagement.Contracts.Models;
 using MarginTrading.AccountsManagement.InternalModels.Interfaces;
 
