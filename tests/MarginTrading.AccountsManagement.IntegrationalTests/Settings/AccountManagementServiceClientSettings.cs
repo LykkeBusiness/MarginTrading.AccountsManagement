55c2c4a0166ace716fdf3c56e497b6ba59206385
atarutin <andrey.tarutin@lykke-business.ch>
2023-06-26 13:21:00 +0300

    Remove old outdated integration tests

--- tests/MarginTrading.AccountsManagement.IntegrationalTests/Settings/AccountManagementServiceClientSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MarginTrading.AccountsManagement.IntegrationalTests.Settings
-{
-    internal class AccountManagementServiceClientSettings
-    {
-        public string ServiceUrl { get; set; }
-    }
-}
