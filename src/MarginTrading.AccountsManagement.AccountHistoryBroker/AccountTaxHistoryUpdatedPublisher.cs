d18ae2e5b92ee2504fd6de4a2426e3f2d5840d27
Fatih Dumanli <fatihdumanli@live.com>
2023-08-24 11:45:41 +0300

    feat(LT-4804): update rabbitMqBroker package and use strategy from the package

--- src/MarginTrading.AccountsManagement.AccountHistoryBroker/AccountTaxHistoryUpdatedPublisher.cs
+++ src/MarginTrading.AccountsManagement.AccountHistoryBroker/AccountTaxHistoryUpdatedPublisher.cs
@@ -30,7 +30,7 @@ namespace MarginTrading.AccountsManagement.AccountHistoryBroker
             _publisher = new RabbitMqPublisher<AccountTaxHistoryUpdatedEvent>(
                 _loggerFactory, _settings.RabbitMq.AccountTaxHistoryUpdated)
                 .SetSerializer(new MessagePackMessageSerializer<AccountTaxHistoryUpdatedEvent>())
-                .SetPublishStrategy(new TopicPublishStrategy(_settings.RabbitMq.AccountTaxHistoryUpdated))
+                .SetPublishStrategy(new PropertiesWithMessageTypeTopicPublishStrategy(_settings.RabbitMq.AccountTaxHistoryUpdated))
                 .DisableInMemoryQueuePersistence()
                 .PublishSynchronously();
 
